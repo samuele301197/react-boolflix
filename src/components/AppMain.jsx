bd3e9a308eea290deb658d88487fe9951e89d63a
samuele301197 <parisisamu32@gmail.com>
2025-06-18 16:34:14 +0200

    feat:hover

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -11,25 +11,21 @@ const AppMain = () => {
                 <div className="row g-4">
                     {film.map((curFilm) => (
                         <div className="col-sm-6 col-md-4 col-lg-3" key={curFilm.id}>
-                            <div className="card h-100">
+                            <div className="card film-card position-relative overflow-hidden">
                                 <img
                                     src={`https://image.tmdb.org/t/p/w342${curFilm.poster_path}`}
                                     className="card-img-top"
-                                    alt={curFilm.title}
-                                />
-                                <div className="card-body d-flex flex-column">
-                                    <h5 className="card-title">Titolo: {curFilm.title}</h5>
-                                    <h6 className="card-subtitle mb-2 text-muted">
-                                        Titolo originale: {curFilm.original_title}
-                                    </h6>
+                                    alt={curFilm.title} />
+                                <div className="card-overlay position-absolute top-0 start-0 w-100 h-100 d-flex flex-column justify-content-center align-items-center text-white text-center p-3">
+                                    <h5 className="card-title">{curFilm.title}</h5>
+                                    <h6 className="card-subtitle mb-2">
+                                        Titolo originale: {curFilm.original_title}</h6>
                                     <p className="card-text">
-                                        Lingua: <FlagsConverter languageFlag={curFilm.original_language} />
-                                    </p>
+                                        Lingua: <FlagsConverter languageFlag={curFilm.original_language} /></p>
                                     <p className="card-text">
                                         Voto:
                                         {'★'.repeat(Math.round(curFilm.vote_average / 2)) +
-                                            '☆'.repeat(5 - Math.round(curFilm.vote_average / 2))}
-                                    </p>
+                                        '☆'.repeat(5 - Math.round(curFilm.vote_average / 2))}</p>
                                 </div>
                             </div>
                         </div>
@@ -38,13 +34,13 @@ const AppMain = () => {
                 <div className="row g-4">
                     {series.map((curSerie) => (
                         <div className="col-sm-6 col-md-4 col-lg-3" key={curSerie.id}>
-                            <div className="card h-100">
+                            <div className="card film-card position-relative overflow-hidden">
                                 <img
                                     src={`https://image.tmdb.org/t/p/w342${curSerie.poster_path}`}
                                     className="card-img-top"
                                     alt={curSerie.title}
                                 />
-                                <div className="card-body d-flex flex-column">
+                                <div className="card-overlay position-absolute top-0 start-0 w-100 h-100 d-flex flex-column justify-content-center align-items-center text-white text-center p-3">
                                     <h5 className="card-title">Titolo: {curSerie.title}</h5>
                                     <h6 className="card-subtitle mb-2 text-muted">
                                         Titolo originale: {curSerie.original_title}
